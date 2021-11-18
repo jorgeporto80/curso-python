bc1749a6d30aa21b1f5053f266e3b6f1831408df
jorgeporto80 <jorgeporto80@gmail.com>
2021-11-18 17:45:48 -0300

    Clase4

--- Clases/src/clases.py
+++ Clases/src/clases.py
@@ -118,3 +118,86 @@ print(mi_lista1)
 print(id(mi_lista2))
 print(type(mi_lista2))
 print(mi_lista2)
+
+numero = 2
+
+# if numero == 1:
+#     print("1")
+# elif numero == 2:
+#     print("2")
+# elif numero == 3:
+#     print("3")
+
+
+# if numero > 3:
+#      print("Si 3")
+# elif numero > 4:
+#     print("si 4")
+# else:
+#     print("no")
+
+
+# Ternario
+numero = 2
+valor =  "Es mayor a 3" if numero > 3 else "No es mayor a 3"
+
+
+print(valor)
+
+
+
+
+# cadena = "Hola mundo"
+# cadena = [1,2,3,4,5]
+
+# suma = 0
+
+# for elemento in cadena:
+#     suma += elemento
+#     print(suma)
+
+# print("Resultado final:",suma)
+
+
+#range("valor inicial", "hasta ....hasta -1", "saltos")
+
+# for vuelta in range(5):
+#     if vuelta == 8:
+#         print("brake")
+#         break
+#     print("doy vueltas", vuelta)
+# else: 
+#     print("Terminé")
+
+
+# mi_lista = [[1,2,3],[4,5,6],[7,8,9]]
+
+
+# for elemento_lista in mi_lista[0:2]:
+#     print(elemento_lista)
+#     for sublementos in elemento_lista:
+#         print(sublementos)
+
+
+mi_lista = [1,2,3,4,4,5,6]
+elemento = 0 
+indice = 0
+
+while elemento != 4:
+
+    elemento = mi_lista[indice] 
+    indice += 1    
+    
+    if elemento == 2:
+        continue
+    
+
+    print(elemento)
+
+
+    if indice == len(mi_lista) -1:
+        print("No encontre el 4")
+        break
+else:
+    print("Lo encontre")
+
